45325cc171d9ae8ae625b60e5c3988fc9a3419a6
Michal-Sitarz <me@michalsitarz.com>
2020-03-12 09:58:54 +0000

    fix: username for repo publish

--- Napier-ConnectFour-Csharp/Program.cs
+++ Napier-ConnectFour-Csharp/Program.cs
@@ -6,7 +6,7 @@ namespace Napier_ConnectFour_Csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello GitHub!");
         }
     }
 }
